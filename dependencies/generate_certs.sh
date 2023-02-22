e8033b68e09b2862e96fa24140032d28e19db25e
Tóth Ádám <toth.adam@thisisdone.com>
2023-02-22 20:58:34 +0100

    run generation independently

--- dependencies/generate_certs.sh
+++ dependencies/generate_certs.sh
@@ -3,14 +3,12 @@
 HOSTNAMES=""
 
 for host in $HOST_NAMES; do
-	HOSTNAMES="$HOSTNAMES -d $host"
-done
-
-certbot certonly \
+	certbot certonly \
     --dns-cloudflare \
     --dns-cloudflare-credentials /dependencies/cloudflare.ini \
-    $HOSTNAMES \
+    -d $host \
 	--config-dir /etc/nginx/certs \
     --email $CF_EMAIL \
     --agree-tos \
     --server https://acme-v02.api.letsencrypt.org/directory
+done
